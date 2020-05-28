d08bee3d0f0f0a44a65b4040cdc977f008624845
S. M. JAHANGIR <smjxpro@gmail.com>
2020-05-28 19:41:20 +0600

    checkout and charge GET redirect

--- payment/views.py
+++ payment/views.py
@@ -16,44 +16,50 @@ def checkout(request):
         context = {'amount': amount,
                    'booking_pk': booking_pk,
                    }
-    return render(request, 'payment/checkout.html', context)
+        return render(request, 'payment/checkout.html', context)
+    else:
+        return redirect(reverse('home'))
 
 
 def charge(request):
-    print('Data:', request.POST)
+    if request.method == 'POST':
 
-    booking_pk = request.POST['booking-pk']
+        print('Data:', request.POST)
 
-    amount = int(float(request.POST['amount']))
+        booking_pk = request.POST['booking-pk']
 
-    booking = get_object_or_404(Booking, pk=booking_pk)
+        amount = int(float(request.POST['amount']))
 
-    if not booking.paid:
-        customer = stripe.Customer.create(
-            email=request.POST['email'],
-            name=request.POST['name'],
-            source=request.POST['stripeToken']
-        )
+        booking = get_object_or_404(Booking, pk=booking_pk)
 
-        charge = stripe.Charge.create(
-            customer=customer,
-            amount=amount * 100,
-            currency='usd',
-            description=f'Payment for booking: {booking}',
+        if not booking.paid:
+            customer = stripe.Customer.create(
+                email=request.POST['email'],
+                name=request.POST['name'],
+                source=request.POST['stripeToken']
+            )
 
-        )
+            charge = stripe.Charge.create(
+                customer=customer,
+                amount=amount * 100,
+                currency='usd',
+                description=f'Payment for booking: {booking}',
 
-        booking.paid = True
-        booking.booked = True
+            )
 
-        booking.room.available = False
-        booking.room.save(force_update=True)
-        booking.save(force_update=True)
+            booking.paid = True
+            booking.booked = True
 
-        # context = {
-        #     'booking': booking,
-        #     'amount': amount,
-        # }
-        # return render(request, 'payment/charge.html', context)
+            booking.room.available = False
+            booking.room.save(force_update=True)
+            booking.save(force_update=True)
 
-    return redirect(reverse('customer:dashboard'))
+            # context = {
+            #     'booking': booking,
+            #     'amount': amount,
+            # }
+            # return render(request, 'payment/charge.html', context)
+
+        return redirect(reverse('customer:dashboard'))
+    else:
+        return redirect(reverse('home'))
